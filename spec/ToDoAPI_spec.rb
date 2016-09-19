c0a982e9f4f26523aaac556a7ff10148accd5fd6
johnmetcalfe <john.metcalfe@skybettingandgaming.com>
2016-09-19 16:31:48 +0100

    Added first post test

--- spec/ToDoAPI_spec.rb
+++ spec/ToDoAPI_spec.rb
@@ -5,6 +5,8 @@ describe HTTParty do
   todo1 = HTTParty.post('http://lacedeamon.spartaglobal.com/todos', query:{title: "Test todo 1", due: Date.today + 1 })
   todo2 = HTTParty.post('http://lacedeamon.spartaglobal.com/todos', query:{title: "Test todo 2", due: Date.today + 1 })
   todo3 = HTTParty.post('http://lacedeamon.spartaglobal.com/todos', query:{title: "Test todo 3", due: Date.today + 1 })
+  todo4 = HTTParty.post('http://lacedeamon.spartaglobal.com/todos', query:{title: "Test todo 4", due: Date.today + 1 })
+  todo5 = HTTParty.post('http://lacedeamon.spartaglobal.com/todos', query:{title: "Test todo 5", due: Date.today + 1 })
 
 
   ###  GET Methods Testing
@@ -48,11 +50,11 @@ describe HTTParty do
 
   it "Update whole item with valid ID" do
 
-    put = HTTParty.put('http://lacedeamon.spartaglobal.com/todos/7956', query:{title: "Hello John", due: "24-09-2016"})
+    put = HTTParty.put("http://lacedeamon.spartaglobal.com/todos/#{todo2['id']}", query:{title: "Hello John", due: Date.today + 4})
     expect(put.code).to eq 200
     expect(put.message).to eq "OK"
     expect(put['title']).to eq "Hello John"
-    expect(put['due']).to eq "2016-09-24"
+    expect(put['due']).to eq "#{Date.today + 4}"
 
 
   end
@@ -83,7 +85,7 @@ describe HTTParty do
 
   it "update with nil values including title" do
 
-    put4 = HTTParty.put('http://lacedeamon.spartaglobal.com/todos/7592', query:{title: nil, due: nil})
+    put4 = HTTParty.put("http://lacedeamon.spartaglobal.com/todos/#{todo2['id']}", query:{title: nil, due: nil})
     expect(put4.code).to eq 400
     expect(put4.message).to eq "Bad Request. Title Not Given."
 
@@ -91,16 +93,16 @@ describe HTTParty do
 
   it "update with nil values in due date" do
 
-    put5 = HTTParty.put('http://lacedeamon.spartaglobal.com/todos/7950', query:{title: "Tester", due: nil})
+    put5 = HTTParty.put("http://lacedeamon.spartaglobal.com/todos/#{todo3['id']}", query:{title: "Tester", due: nil})
     expect(put5.code).to eq 200
     expect(put5.message).to eq "OK"
-    expect(put5.due).to eq (Date.Today + 1)
+    expect(put5['due']).to eq (Date.Today + 1)
 
   end
 
   it "Update with date in past" do
 
-    put6 = HTTParty.put('http://lacedeamon.spartaglobal.com/todos/7934', query:{title: "Tester", due: "01-01-1970"})
+    put6 = HTTParty.put("http://lacedeamon.spartaglobal.com/todos/#{todo4['id']}", query:{title: "Tester", due: "01-01-1970"})
     expect(put6.code).to eq 400
     expect(put6.message).to eq "Bad Request. Date Not In Future."
 
@@ -108,19 +110,15 @@ describe HTTParty do
 
 
   ###  DELETE METHODS TESTS
-=begin
   it "Delete with valid ID" do
 
-    del1 = HTTParty.delete('http://lacedeamon.spartaglobal.com/todos/7902')
+    del1 = HTTParty.delete("http://lacedeamon.spartaglobal.com/todos/#{todo5['id']}")
     expect(del1.code).to eq 204
     expect(del1.message).to eq "No Content"
-    get1 = HTTParty.get('http://lacedeamon.spartaglobal.com/todos/7902')
+    get1 = HTTParty.get("http://lacedeamon.spartaglobal.com/todos/#{todo5['id']}")
     expect(get1.code).to eq 404
     expect(get1.message).to eq "Not Found"
-    HTTParty.post('http://lacedeamon.spartaglobal.com/todos', query:{id: 7902, title: "Hello John", due: "24-09-2016"})
-
   end
-=end
 
   it "Delete with invalid ID" do
 
@@ -146,13 +144,27 @@ describe HTTParty do
 
   it "Valid creating new todo" do
 
+    post1 = HTTParty.post('http://lacedeamon.spartaglobal.com/todos', query:{title: "Test POST 1", due: Date.today + 1 })
+    expect(post1.code).to eq 201
+    expect(post1.message).to eq "Created"
+    expect(post1['title']).to eq "Test POST 1"
+    expect(post1['due']).to eq "#{Date.today + 1}"
+
+
   end
 
   it "Try to create against todos/id" do
 
+    post2 = HTTParty.post("http://lacedeamon.spartaglobal.com/todos/#{todo1['id']}", query:{title: "Test POST 2", due: Date.today + 1 })
+    expect(post2.code).to eq 405
+    expect(post2.message).to eq "Method Not Allowed"
+    expect(post2.body).to eq "Method Not Allowed. To create a new todo, POST to the collection, not an item within it."
+
   end
 
   it "Try to create with invalid parameters" do
+
+
 
   end
 
