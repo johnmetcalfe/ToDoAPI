9f07c391c50f340691cc8711fc6a66a2b41a969d
johnmetcalfe <john.metcalfe@skybettingandgaming.com>
2016-09-19 14:29:40 +0100

    Fixed no method error

--- spec/ToDoAPI_spec.rb
+++ spec/ToDoAPI_spec.rb
@@ -16,11 +16,17 @@ describe HTTParty do
     r = HTTParty.get('http://lacedeamon.spartaglobal.com/todos/7960')
     expect(r.code).to eq 200
     expect(r.message).to eq "OK"
-    expect(r.id).to eq 7960
-    expect(r.title).to eq "Alex Test Two"
-    expect(r.due).to eq "2028-01-16"
-    expect(r.created_at).to eq "2016-09-19T12:55:10.855Z"
-    expect(r.updated_at).to eq "2016-09-19T12:55:10.855Z"
+    expect(r['id']).to eq 7960
+    expect(r['title']).to eq "Alex Test Two"
+    expect(r['due']).to eq "2028-01-16"
+    expect(r['created_at']).to eq "2016-09-19T12:55:10.855Z"
+    expect(r['updated_at']).to eq "2016-09-19T12:55:10.855Z"
+
+  end
+
+  it "get invalid ID todo" do
+
+    r = HTTParty.get('http://lacedeamon.spartaglobal.com/todos/9999')
 
   end
 
