592a61e746936dfce06651a620fff56b96357c26
Alfie Thornton <alfred.thornton@skybettingandgaming.com>
2016-09-20 09:38:40 +0100

    started helper methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
@@ -12,3 +11,15 @@ end
 require 'rspec'
 require 'httparty'
 require 'date'
+
+def url(path)
+  ('http://lacedeamon.spartaglobal.com' + path)
+end
+
+def create_todo
+
+end
+
+def delete_all
+
+end
